01c6e5cbe51b176d6e33cf7871fa1ff97a80b7b4
Gastón I. Silva E <silvag777@gmail.com>
2015-03-06 13:55:04 -0600

    pd-editor receives the arg isPostfix

--- addon/views/pd-editor.js
+++ addon/views/pd-editor.js
@@ -8,9 +8,28 @@ export default Ember.View.extend({
 
   html: null,
 
+  preview: true,
+
+  idPostfix: '',
+  wmdButtonBarId: null, 
+  wmdInputId: null, 
+  wmdPreviewId: null, 
+
+  _idPostfix: function() {
+    var idPostfix = this.get('idPostfix');
+    this.set('wmdButtonBarId', 'wmd-button-bar' + idPostfix);
+    this.set('wmdInputId', 'wmd-input' + idPostfix);
+    this.set('wmdPreviewId', 'wmd-preview' + idPostfix);
+  }.on('init'),
+
   didInsertElement : function() {
-    var converter = this.pagedownService.converter,
-        idPostfix = null,
+    this._super();
+    Ember.run.scheduleOnce('afterRender', this, this.afterRenderEvent); 
+  },
+
+  afterRenderEvent: function() {
+    var converter = this.pagedownService.sanitizedConverter,
+        idPostfix = this.get('idPostfix'),
         options = {};
 
     var editor = new this.pagedownService.Editor(converter, idPostfix, options);
@@ -19,3 +38,4 @@ export default Ember.View.extend({
   } 
 
 });
+
--- tests/unit/views/pd-editor-test.js
+++ tests/unit/views/pd-editor-test.js
@@ -5,8 +5,41 @@ import {
 
 moduleFor('view:pd-editor');
 
-// Replace this with your real tests.
 test('it exists', function(assert) {
   var view = this.subject();
   assert.ok(view);
 });
+
+test('no idPostfix', function(assert) {
+  var view = this.subject();
+  assert.equal(view.get('wmdButtonBarId'), 'wmd-button-bar');
+  assert.equal(view.get('wmdInputId'), 'wmd-input');
+  assert.equal(view.get('wmdPreviewId'), 'wmd-preview');
+});
+
+test('idPostfix = second', function(assert) {
+  var view = this.subject({
+    idPostfix: '-second'
+  });
+  assert.equal(view.get('wmdButtonBarId'), 'wmd-button-bar-second');
+  assert.equal(view.get('wmdInputId'), 'wmd-input-second');
+  assert.equal(view.get('wmdPreviewId'), 'wmd-preview-second');
+});
+
+test('idPostfix = \'\'', function(assert) {
+  var view = this.subject({
+    idPostfix: '' 
+  });
+  assert.equal(view.get('wmdButtonBarId'), 'wmd-button-bar');
+  assert.equal(view.get('wmdInputId'), 'wmd-input');
+  assert.equal(view.get('wmdPreviewId'), 'wmd-preview');
+});
+
+test('idPostfix can be set only at init', function(assert) {
+  var view = this.subject();
+  view.set('idPostfix', 'foobar');
+  assert.equal(view.get('wmdButtonBarId'), 'wmd-button-bar');
+  assert.equal(view.get('wmdInputId'), 'wmd-input');
+  assert.equal(view.get('wmdPreviewId'), 'wmd-preview');
+});
+
