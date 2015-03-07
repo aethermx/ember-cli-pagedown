3d7eb68cc7c3b553ab697088b85e8fc736fc4a51
Gastón I. Silva E <silvag777@gmail.com>
2015-03-07 02:20:01 -0600

    properly export the initializer

---
+++ app/initializers/app-pagedown-service.js
@@ -0,0 +1,6 @@
+import { initialize } from 'ember-cli-pagedown/initializers/pagedown-service';
+
+export default {
+  name: 'app-pagedown-service',
+  initialize: initialize
+};
--- app/initializers/pagedown-service.js
+++
@@ -1,3 +0,0 @@
-import pagedownService from 'ember-cli-pagedown/initializers/pagedown-service';
-
-export default pagedownService;
