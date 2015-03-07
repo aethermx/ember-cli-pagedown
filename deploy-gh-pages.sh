5fda40a88c2e4ff32fed4686cda4b12cc7a44b80
Gastón I. Silva E <silvag777@gmail.com>
2015-03-07 11:38:54 -0600

    use a Bootstrap container

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -4,4 +4,11 @@ ember build --environment='production'
 
 cp -Rv dist/* ../ember-cli-pagedown-gh-pages/
 
+cd ../ember-cli-pagedown-gh-pages/
+
+git add -u .
+
+git commit -m 'update'
+
+git push -u origin gh-pages
 
